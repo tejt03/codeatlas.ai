cdfde4fd7990350fb46f2938eea8718617fdec57
Tej Tilekar <ttileka@calstatela.edu>
2026-02-21 12:53:45 -0800

    Restrict users :id route to ObjectId

--- snippet-server/api/users/user.routes.js
+++ snippet-server/api/users/user.routes.js
@@ -3,16 +3,16 @@ const router = require('express').Router();
 const controller = require('./user.controller');
 const middleware = require('../middleware/authorization');
 
-// GET /users/:id
-router.get('/:id', controller.getUserById);
+// POST /users/login
+router.post('/login', controller.loginUser);
 
 // POST /users
 router.post('/register', controller.registerUser);
 
-// POST /users/login
-router.post('/login', controller.loginUser);
+// GET /users/:id
+router.get('/:id([0-9a-fA-F]{24})', controller.getUserById);
 
 // PUT /users/:id
-router.put('/:id', middleware.verifyToken, controller.updateUser);
+router.put('/:id([0-9a-fA-F]{24})', middleware.verifyToken, controller.updateUser);
 
 module.exports = router;
